daf4d69a7665abea4377a21c1e9c22325bed2c16
Likaifeng <likaifeng2011@gmail.com>
2020-12-11 15:53:55 +0800

    改一下算法

--- 查看WAZ分布情况.R
+++ 查看WAZ分布情况.R
@@ -11,16 +11,24 @@ load("D:/Rdata/hmMetabo/hmMetabo/data/bm.m2MBMI.Rdata")
 load("D:/Rdata/hmMetabo/hmMetabo/data/bm.m2Izs.Rdata")
 
 # 查看二月龄所有样本中WAZ大于1或者小于-1的样本量
-length(bm.m2MonBMI$wfaz[bm.m2MonBMI$wfaz>1]) # 67
-length(bm.m2MonBMI$wfaz[bm.m2MonBMI$wfaz<-1]) # 1
+wfaz2_1 <- bm.m2MonBMI %>%
+  filter(wfaz < -1)
+wfaz2_2 <- bm.m2MonBMI %>%
+  filter(wfaz > 1)
+
 
 # 以母亲BMI筛选数据，看一下WAZ大于1或者小于-1的样本量
-length(bm.m2MBMI$wfaz[bm.m2MBMI$wfaz>1]) # 32
-length(bm.m2MBMI$wfaz[bm.m2MBMI$wfaz<-1]) # 1
+wfaz3_1 <- bm.m2MBMI %>%
+  filter(wfaz < -1)
+wfaz3_2 <- bm.m2MBMI %>%
+  filter(wfaz > 1)
+
 
 # 以LAZ筛选数据，看一下WAZ大于1或者小于-1的样本量
-length(bm.m2Izs$wfaz[bm.m2Izs$wfaz>1]) # 42
-length(bm.m2Izs$wfaz[bm.m2Izs$wfaz<-1]) # 1
+wfaz4_1 <- bm.m2Izs %>%
+  filter(wfaz < -1)
+wfaz4_2 <- bm.m2Izs %>%
+  filter(wfaz > 1)
 
 # TODO: 二月龄样本中WAZ小于-1的样本量只有1个
 
@@ -34,15 +42,21 @@ load("D:/Rdata/hmMetabo/hmMetabo/data/bm.m6MBMI.Rdata")
 load("D:/Rdata/hmMetabo/hmMetabo/data/bm.m6Izs.Rdata")
 
 # 查看6月龄所有样本中WAZ大于1或者小于-1的样本量
-length(bm.m6MonBMI$wfaz[bm.m6MonBMI$wfaz>1]) # 73
-length(bm.m6MonBMI$wfaz[bm.m6MonBMI$wfaz<-1]) # 1
+wfaz5_1 <- bm.m6MonBMI %>%
+  filter(wfaz < -1)
+wfaz5_2 <- bm.m6MonBMI %>%
+  filter(wfaz > 1)
 
 # 以母亲BMI筛选数据，看一下WAZ大于1或者小于-1的样本量
-length(bm.m6MBMI$wfaz[bm.m6MBMI$wfaz>1]) # 48
-length(bm.m6MBMI$wfaz[bm.m6MBMI$wfaz<-1]) # 1
+wfaz6_1 <- bm.m6MBMI %>%
+  filter(wfaz < -1)
+wfaz6_2 <- bm.m6MBMI %>%
+  filter(wfaz > 1)
 
 # 以LAZ筛选数据，看一下WAZ大于1或者小于-1的样本量
-length(bm.m6Izs$wfaz[bm.m6Izs$wfaz>1]) # 51
-length(bm.m6Izs$wfaz[bm.m6Izs$wfaz<-1]) # 1
+wfaz7_1 <- bm.m6Izs %>%
+  filter(wfaz < -1)
+wfaz7_2 <- bm.m6Izs %>%
+  filter(wfaz > 1)
 
 # TODO: WAZ小于-1的样本量只有1个
